7a45a1d0aa58661ca8f92aa1c2cc8179e4ea9918
mdanahy <mdanahy@protonmail.com>
2022-12-04 19:34:58 -0700

    Logo adds

---
+++ logo1.py
@@ -0,0 +1,50 @@
+import turtle
+import math
+
+turtle.title('Hi! I\'m Bob the turtle!')
+turtle.setup(width=1000, height=1000)
+
+bob = turtle.Turtle(shape='turtle')
+
+bob.left(90)
+bob.forward(80)
+bob.right(90)
+bob.forward(80)
+bob.right(90)
+bob.forward(80)
+bob.right(90)
+bob.forward(80)
+bob.right(90)
+
+bob.left(90)
+bob.forward(80)
+bob.right(90)
+bob.forward(80)
+bob.right(90)
+bob.forward(80)
+bob.right(90)
+bob.forward(80)
+bob.right(90)
+
+bob.left(90)
+bob.forward(80)
+bob.right(90)
+bob.forward(80)
+bob.right(90)
+bob.forward(80)
+bob.right(90)
+bob.forward(80)
+bob.right(90)
+
+bob.left(90)
+bob.forward(80)
+bob.right(90)
+bob.forward(80)
+bob.right(90)
+bob.forward(80)
+bob.right(90)
+bob.forward(80)
+bob.right(90)
+
+turtle.exitonclick()
+
